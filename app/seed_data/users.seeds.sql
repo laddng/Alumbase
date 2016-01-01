1753f3c6817ea12afe02f75a5852ade24323c957
Nick Ladd <laddng12@wfu.edu>
2016-01-01 18:58:25 -0500

    private to public

--- app/seed_data/users.seeds.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `users` (`user_id`, `first_name`, `last_name`, `major_id`, `graduation_year`, `email_address`) VALUES
-(59, 'John', 'Marbach', 11, 2015, 'jmarb@gmail.com'),
-(60, 'Zach', 'Klein', 39, 2004, 'zach@vimeo.com'),
-(61, 'James', 'Beshara', 12, 2007, 'james@tilt.com'),
-(62, 'David', 'Hughes', 11, 2015, 'dwhughes@gmail.com');
